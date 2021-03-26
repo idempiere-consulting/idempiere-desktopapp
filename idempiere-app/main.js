bfc5d292f47254f6b5eaa8e028e82ea060b334ba
VoyagerClass <vincenzo.distaso@stud.tecnicosuperiorekennedy.it>
2021-03-26 15:08:14 +0100

    start idempiere-app :)

---
+++ idempiere-app/main.js
@@ -0,0 +1,112 @@
+const electron = require('electron');
+const url = require ('url');
+const path = require ('path');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWindow;
+
+app.on('ready', function(){
+    // create new window
+    mainWindow = new BrowserWindow({
+        width:1000,
+        height:700,
+        webPreferences:{
+            nodeIntegration:true,
+            contextIsolation: false
+        }
+    });
+    //load html file into the window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    app.on('closed', function(){
+        app.quit();
+    });
+    
+    //build menu from mainMenu
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //insert menu
+    Menu.setApplicationMenu(mainMenu);
+    
+});
+
+function viewCRMWindow(){
+    
+    //load html file into the window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'CRMWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    
+
+}
+
+//carica la pagina mainWindow.html su schermo
+function viewMainWindow(){
+    
+    //load html file into the window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    
+
+}
+
+const mainMenuTemplate = [
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'CRM',
+                click(){
+                    viewCRMWindow();
+                }
+            },
+            {
+                label: 'Main',
+                click(){
+                    viewMainWindow();
+                }
+            },
+            {
+                label: 'Quit',
+                accellerator: process.platform == 'darwin' ? 'Command+Q' : 'Control+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+//if mac, add empty object to menu
+
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+//add dev tools if not in production
+
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [{
+                label: 'Toggle DevTools',
+                accellerator: process.platform == 'darwin' ? 'Command+I' : 'Control+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+            
+        ]
+    });
+}
