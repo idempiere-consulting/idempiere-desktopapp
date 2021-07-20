a7046a2adfbe774edf427e22514b2619aa533330
idempiere-consulting <info@idempiere.consulting>
2021-07-20 13:51:09 +0200

    font

--- desktop_app_v2/js/Ticket(P)/DetailsTicketWindow.js
+++ desktop_app_v2/js/Ticket(P)/DetailsTicketWindow.js
@@ -23,17 +23,71 @@ function getTicket() {
 
 
             a = data.records;
-            console.log(a[0].DocumentNo);
+            var numDoc='';
+            var businessPartner='';
+            var priority='';
+            var priorityUser='';
+            var dueType='';
+            var requestType='';
+            var description='';
+            var help ='';
+            var summary='';
 
-            document.getElementById('ndoc').value = a[0].DocumentNo;
-            document.getElementById('bp').value = a[0].C_BPartner_ID.identifier;
-            document.getElementById('priority').value = a[0].Priority.identifier;
-            document.getElementById('priorityuser').value = a[0].PriorityUser.identifier;
-            document.getElementById('scandenzaType').value = a[0].DueType.identifier;
-            document.getElementById('requestType').value = a[0].R_RequestType_ID.identifier;
-            document.getElementById('description').value = a[0].Description;
-            document.getElementById('help').value = a[0].Help;
-            document.getElementById('summary').value = a[0].Summary;
+            if (a[0].DocumentNo == undefined) {
+                numDoc ='';
+            }
+            else{numDoc = a[0].DocumentNo}
+
+            if (a[0].C_BPartner_ID == undefined) {
+                businessPartner ='';
+            }
+            else{businessPartner = a[0].C_BPartner_ID.identifier}
+
+            if (a[0].Priority == undefined) {
+                priority ='';
+            }
+            else{priority = a[0].Priority.identifier}
+
+            if (a[0].PriorityUser == undefined) {
+                priorityUser ='';
+            }
+            else{priorityUser = a[0].PriorityUser.identifier}
+
+            if (a[0].DueType == undefined) {
+                dueType ='';
+            }
+            else{dueType = a[0].DueType.identifier}
+
+            if (a[0].R_RequestType_ID == undefined) {
+                requestType ='';
+            }
+            else{requestType = a[0].R_RequestType_ID.identifier}
+
+            if (a[0].Description == undefined) {
+                description ='';
+            }
+            else{description = a[0].Description}
+
+            if (a[0].Help == undefined) {
+                var help ='';
+            }
+            else{help = a[0].Help}
+
+            if (a[0].Summary == undefined) {
+                var summary ='';
+            }
+            else{summary = a[0].Summary}
+
+
+            document.getElementById('ndoc').value = numDoc;
+            document.getElementById('bp').value = businessPartner;
+            document.getElementById('priority').value = priority;
+            document.getElementById('priorityuser').value = priorityUser;
+            document.getElementById('scandenzaType').value = dueType;
+            document.getElementById('requestType').value = requestTyper;
+            document.getElementById('description').value = description;
+            document.getElementById('help').value = help;
+            document.getElementById('summary').value = summary;
 
 
 
