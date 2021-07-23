cb4b62a293fe8f4782280b75975127c408c81450
idempiere-consulting <info@idempiere.consulting>
2021-07-23 11:12:09 +0200

    Menu dinamico

--- desktop_app_v2/js/filejscommon.js
+++ desktop_app_v2/js/filejscommon.js
@@ -1,7 +1,182 @@
 const electron_2 = window.require("electron");
 const ipcRender_2 = electron_2.ipcRenderer;
-
 document.getElementById("span_usernmane_jslogic").innerHTML = ipcRender_2.sendSync('send:user');
+
+//json che contine tutti i dati delle pagine
+var JsonMenu = {
+    "Menu": [{
+            "menu": "CRM",
+            "icon": ["fas", "fa-users"],
+            "sottoMenu": [{
+                    "nomeSottoMenu": "Leads",
+                    "pathSottoMenu": "CRM/Leads.html"
+                },
+                {
+                    "nomeSottoMenu": "Opportunità",
+                    "pathSottoMenu": "CRM/Opportunita.html"
+                },
+                {
+                    "nomeSottoMenu": "Bacheca",
+                    "pathSottoMenu": "CRM/Bacheca.html"
+                }
+            ],
+            "pathMenu": ""
+        },
+        {
+            "menu": "Service(U)",
+            "icon": ["fas", "fa-hands-helping"],
+            "sottoMenu": [{
+                    "nomeSottoMenu": "OrdiniLavoro",
+                    "pathSottoMenu": "Service(U)/OrdiniLavoro.html"
+                },
+                {
+                    "nomeSottoMenu": "Calendario",
+                    "pathSottoMenu": "Service(U)/Calendario.html"
+                },
+                {
+                    "nomeSottoMenu": "Anomalie",
+                    "pathSottoMenu": "Service(U)/Anomalie.html"
+                }
+            ],
+            "pathMenu": ""
+        },
+        {
+            "menu": "Service(P)",
+            "icon": ["fas", "fa-hands-helping"],
+            "sottoMenu": [{
+                    "nomeSottoMenu": "Anomalia",
+                    "pathSottoMenu": "Service(P)/Anomalia.html"
+                },
+                {
+                    "nomeSottoMenu": "Rich.Manutenzione",
+                    "pathSottoMenu": "Service(P)/Rich.Manutenzione.html"
+                }
+            ],
+            "pathMenu": ""
+        },
+        {
+            "menu": "Fatture",
+            "icon": ["fas", "fa-file-invoice-dollar"],
+            "sottoMenu": [{
+                    "nomeSottoMenu": "Acquisto",
+                    "pathSottoMenu": "Fatture/Acquisto.html"
+                },
+                {
+                    "nomeSottoMenu": "Vendita",
+                    "pathSottoMenu": "Fatture/Vendita.html"
+                }
+            ],
+            "pathMenu": ""
+        },
+        {
+            "menu": "Ordini Di Vendita",
+            "icon": ["fas", "fa-clipboard-list"],
+            "sottoMenu": [{
+                    "nomeSottoMenu": "VediODV",
+                    "pathSottoMenu": "OrdiniDiVendita/VediODV.html"
+                }
+
+            ],
+            "pathMenu": ""
+        },
+        {
+            "menu": "Ticket(P)",
+            "icon": ["fas", "fa-ticket-alt"],
+            "sottoMenu": [{
+                    "nomeSottoMenu": "Supporto",
+                    "pathSottoMenu": "Ticket(P)/Supporto.html"
+                },
+                {
+                    "nomeSottoMenu": "Ricerca Sessione",
+                    "pathSottoMenu": "Ticket(P)/RicercaSessione.html"
+                },
+                {
+                    "nomeSottoMenu": "Assegnazione Risorse",
+                    "pathSottoMenu": "Ticket(P)/AssegnazioneRisorse.html"
+                }
+
+            ],
+            "pathMenu": ""
+        },
+        {
+            "menu": "Ticket(I)",
+            "icon": ["fas", "fa-ticket-alt"],
+            "sottoMenu": [{
+                    "nomeSottoMenu": "Helpdesk",
+                    "pathSottoMenu": "Ticket(I)/HelpDesk.html"
+                },
+                {
+                    "nomeSottoMenu": "Dett. Ticket",
+                    "pathSottoMenu": "Ticket(I)/Dett.Ticket.html"
+                },
+                {
+                    "nomeSottoMenu": "Task",
+                    "pathSottoMenu": "Ticket(I)/Task.html"
+                },
+                {
+                    "nomeSottoMenu": "Da Schedulare",
+                    "pathSottoMenu": "Ticket(I)/DaSchedulare.html"
+                },
+                {
+                    "nomeSottoMenu": "Ore",
+                    "pathSottoMenu": "Ticket(I)/Ore.html"
+                }
+
+            ],
+            "pathMenu": ""
+        },
+        {
+            "menu": "Conto Lavoro",
+            "icon": ["fas", "fa-sign-out-alt"],
+            "sottoMenu": [],
+            "pathMenu": "ContoLavoro/ContoLavoro.html"
+        },
+        {
+            "menu": "Acquisti",
+            "icon": ["fas", "fa-shopping-cart"],
+            "sottoMenu": [],
+            "pathMenu": "Acquisti/Acquisti.html"
+        },
+        {
+            "menu": "Task Ore",
+            "icon": ["fas", "fa-clock"],
+            "sottoMenu": [],
+            "pathMenu": "Task-Ore/TaskOre.html"
+        },
+        {
+            "menu": "Produzione",
+            "icon": ["fas", "fa-industry"],
+            "sottoMenu": [],
+            "pathMenu": "Produzione/Produzione.html"
+        },
+        {
+            "menu": "Logistica",
+            "icon": ["fas", "fa-boxes"],
+            "sottoMenu": [{
+                    "nomeSottoMenu": "Prodotti/Giac.",
+                    "pathSottoMenu": "Logistica/ProdottiGiacc.html"
+                },
+                {
+                    "nomeSottoMenu": "Crea DDT CL",
+                    "pathSottoMenu": "Logistica/CreaDDTcl.html"
+                },
+                {
+                    "nomeSottoMenu": "Crea DDT",
+                    "pathSottoMenu": "Logistica/CreaDDT.html"
+                },
+                {
+                    "nomeSottoMenu": "Lista Viaggi",
+                    "pathSottoMenu": "Logistica/ListaViaggi.html"
+                }
+            ],
+            "pathMenu": ""
+        }
+
+
+    ]
+}
+
+CreateMenu();
 PermessiMenu();
 
 function logOut() {
@@ -152,8 +327,6 @@ function OrderTable(nameTbodyTable, nCell) {
             shouldSwitch = false;
             x = table.rows[index].cells[nCell];
             y = table.rows[index + 1].cells[nCell];
-            /* console.log(x.innerHTML + "    " + y.innerHTML);
-            console.log(x.innerHTML < y.innerHTML); */
             if (x.innerHTML < y.innerHTML) {
                 shouldSwitch = true;
                 break;
@@ -164,4 +337,90 @@ function OrderTable(nameTbodyTable, nCell) {
             switching = true;
         }
     }
+}
+
+
+function CreateMenu() {
+
+    var path = require('path');
+    filename = path.basename(__filename);
+    //prendo tutti gli oggetti contenuti in Menu e li metto in una variabile   
+    var arrayMenu = JsonMenu.Menu;
+    //prendo l'elemento nella pagina html con il tag "ul"
+    var dynamicMenu = document.getElementById("dynamicMenu");
+
+    //ciclo su ogni elemento di Menu
+    arrayMenu.forEach(element => {
+        //per ogni elemento di Menu creo un "li" e gli assegno una classe
+        var liMenu = document.createElement("li");
+        liMenu.classList.add("macrocategory-permission-menu");
+
+
+        //creo elemento "i", assegno la classe e lo appendo all'elemento padre "span" 
+        var iconMenu = document.createElement("i");
+        iconMenu.classList.add(...element.icon);
+        iconMenu.style.marginRight = "5px";
+
+        //se la lunghezza del sottomenu non è zero allora esegui quello all'interno
+        if (element.sottoMenu.length != 0) {
+            //creo l'elemento "span" gli assegno una classe e lo appendo all'elemento padre "li"
+            var spanMenu = document.createElement("span");
+            spanMenu.classList.add("opener");
+            liMenu.appendChild(spanMenu);
+            spanMenu.appendChild(iconMenu);
+
+            /*se index.html
+                CRM/LEAds.html
+             else
+                ../CRM/Opportunità.html
+            */
+
+            spanMenu.innerHTML += element.menu;
+
+
+            //creo un elemento "ul"
+            var ulSottomenu = document.createElement("ul");
+            //ciclo ogni elemento del sottomenù
+            element.sottoMenu.forEach(element1 => {
+
+                //creo elemento "li" gli aggiungo la classe
+                var liSottomenu = document.createElement("li");
+                liSottomenu.classList.add("category-permission-menu");
+
+                //creo elemento "a" e gli inserisco la voce "nomeSottoMenu" e gli href
+                var linkSottoMenu = document.createElement("a");
+                linkSottoMenu.innerHTML = element1.nomeSottoMenu;
+                if (filename != "index.html") {
+                    var temp = "../" + element1.pathSottoMenu;
+                    linkSottoMenu.href = temp;
+                } else {
+                    linkSottoMenu.href = element1.pathSottoMenu;
+                }
+
+
+                liSottomenu.appendChild(linkSottoMenu);
+                ulSottomenu.appendChild(liSottomenu);
+                liMenu.appendChild(ulSottomenu);
+            });
+        } else {
+            var linkMenu = document.createElement("a");
+            if (filename != "index.html") {
+                var temp = "../" + element.pathMenu;
+                linkMenu.href = temp;
+            } else {
+                linkMenu.href = element.pathMenu;
+            }
+
+            console.log(iconMenu);
+
+            linkMenu.appendChild(iconMenu);
+            linkMenu.innerHTML += element.menu;
+
+            liMenu.appendChild(linkMenu);
+
+        }
+
+
+        dynamicMenu.appendChild(liMenu);
+    });
 }
